42e4ae346d36c42678246425678dc753d0d8447c
wilson agrela <wilson-andre100@hotmail.com>
2021-04-07 18:54:10 +0100

    organização de pastas e implementação de sass

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express')
-
 const app = express()
+const port = 3000
+
+//isto serve para usar css
+app.use(express.static('public'))
+app.use('/styles', express.static(__dirname + 'public/styles'))
+
 app.set('view-engine', 'ejs')
 app.get('/', (req, res) =>{
 
     res.render('index.ejs')
 })
-app.listen(3000)
+
+app.listen(port)
